e047e1601521382c5679c4cb6247a28902b9f889
MuhammadSaqibShoaib <muhammadsaqib54@outlook.com>
2022-12-12 01:19:18 +0500

    Commit button activiated

--- app/src/main/java/com/example/githubmanual/MainActivity.java
+++ app/src/main/java/com/example/githubmanual/MainActivity.java
@@ -12,17 +12,22 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 Button vcs_btn;
 Button rep_btn;
+Button com_btn;
 TextView MainText,heading;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Getting refernces of views
         MainText = findViewById(R.id.text1);
         heading = findViewById(R.id.heading);
         vcs_btn = findViewById(R.id.btn1);
         rep_btn = findViewById(R.id.btn_2);
+        com_btn = findViewById(R.id.btn_3);
+        // setting on click listener on buttons
         vcs_btn.setOnClickListener(v -> VCS_BTN(v));
         rep_btn.setOnClickListener(v -> REP_BTN(v));
+        com_btn.setOnClickListener(v -> COM_BTN(v));
     }
 
 
@@ -55,10 +60,26 @@ TextView MainText,heading;
         transaction.replace(R.id.container,definationFragment);
         transaction.commit();
     }
+
+    public void COM_BTN(View v){
+        VisiblityRemover(v);
+        // sending data to fragment
+        Bundle bundle = new Bundle();
+        bundle.putString("heading",getString(R.string.COM_Frag));
+        bundle.putString("def",getString(R.string.COM_Def));
+
+        // opening a fragment
+        Fragment definationFragment = new DefinationFragment();
+        definationFragment.setArguments(bundle);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container,definationFragment);
+        transaction.commit();
+    }
     public void VisiblityRemover(View v){
         MainText.setVisibility(v.GONE);
         heading.setVisibility(v.GONE);
         vcs_btn.setVisibility(v.GONE);
         rep_btn.setVisibility(v.GONE);
+        com_btn.setVisibility(v.GONE);
     }
 }
