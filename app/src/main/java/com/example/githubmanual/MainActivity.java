5a157c3ce218a0048ec297401f3f00015a574fb5
MuhammadSaqibShoaib <muhammadsaqib54@outlook.com>
2022-12-12 00:09:50 +0500

    Android studio working + Fragment activation

--- app/src/main/java/com/example/githubmanual/MainActivity.java
+++ app/src/main/java/com/example/githubmanual/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.githubmanual;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+Button vcs_btn;
+TextView MainText,heading;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MainText = findViewById(R.id.text1);
+        heading = findViewById(R.id.heading);
+        vcs_btn = findViewById(R.id.btn1);
+
+        vcs_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                VisiblityRemover(v);
+                Fragment definationFragment = new DefinationFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.container,definationFragment);
+                transaction.commit();
+            }
+        });
+    }
+
+    public void VisiblityRemover(View v){
+        MainText.setVisibility(v.GONE);
+        heading.setVisibility(v.GONE);
+        vcs_btn.setVisibility(v.GONE);
     }
 }
